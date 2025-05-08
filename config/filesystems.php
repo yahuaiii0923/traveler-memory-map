d732366eb44cb377e8a87efe27bb846bc255955d
yahuaiii0923 <D00275358@student.dkit.ie>
2025-05-08 13:39:02 +0100

    minor changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -75,4 +75,21 @@ return [
         public_path('storage') => storage_path('app/public'),
     ],
 
+    'mysql' => [
+        'driver' => 'mysql',
+        'host' => env('DB_HOST', '127.0.0.1'),
+        'port' => env('DB_PORT', '3306'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'strict' => true,
+        'engine' => null,
+        'options' => [
+            PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA', '/path/to/ca-cert.pem'),
+        ],
+    ],
+
 ];
